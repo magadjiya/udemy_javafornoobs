8e4d9a900a6140dfaaeae8bc8cb7d0e62029aa66
magadjiya <khadijaumardh@gmail.com>
2025-05-26 20:40:37 +0100

    division tester improved; remainder is taken into account. check for valid input added

--- Problem6.java
+++ Problem6.java
@@ -11,36 +11,32 @@ public class Problem6 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("What Sort of Math Problems would you like to solve?\nPlease select a symbol for it (+,-,*,/)");
+        System.out.print("What Sort of Math Problems would you like to practice\nPlease select a symbol for it (+,-,*,/) ");
         String symbol = input.next();
 
         Random randomNumbers = new Random();
 
-        int num1, num2, result, count = 5;
+        int num1, num2, result, userResult, divisionRemainder, userRemainder, count = 5;
+
+        // Validate the input symbol
+        // Loop until a valid symbol is entered
+        do {
+            if (symbol.equals("+") || symbol.equals("-") || symbol.equals("*") || symbol.equals("/")) {
+                break;
+            } else {
+                System.out.print("Invalid input. Please enter a valid symbol (+,-,*,/): ");
+                symbol = input.next();
+            }
+        } while (true);
 
         switch (symbol) {
             case "+":
                 while (count > 0) {
-                    num1 = randomNumbers.nextInt(100);
-                    num2 = randomNumbers.nextInt(100);
+                    num1 = randomNumbers.nextInt(10);
+                    num2 = randomNumbers.nextInt(10);
                     result = num1 + num2;
                     System.out.print(num1 + " + " + num2 + " = ");
-                    int userResult = input.nextInt();
-                    if (userResult == result) {
-                        count--;
-                        System.out.println("Correct. " + count + " tries left.");
-                    } else {
-                        System.out.println("Incorrect." + count + " tries left.");
-                    }
-                }
-                break;
-            case "/":
-                while (count > 0) {
-                    num1 = randomNumbers.nextInt(100);
-                    num2 = randomNumbers.nextInt(100);
-                    result = num1 / num2;
-                    System.out.print(num1 + " / " + num2 + " = ");
-                    int userResult = input.nextInt();
+                    userResult = input.nextInt();
                     if (userResult == result) {
                         count--;
                         System.out.println("Correct. " + count + " tries left.");
@@ -51,11 +47,11 @@ public class Problem6 {
                 break;
             case "-":
                 while (count > 0) {
-                    num1 = randomNumbers.nextInt(100);
-                    num2 = randomNumbers.nextInt(100);
+                    num1 = randomNumbers.nextInt(10);
+                    num2 = randomNumbers.nextInt(10);
                     result = num1 - num2;
                     System.out.print(num1 + " - " + num2 + " = ");
-                    int userResult = input.nextInt();
+                    userResult = input.nextInt();
                     if (userResult == result) {
                         count--;
                         System.out.println("Correct. " + count + " tries left.");
@@ -66,11 +62,11 @@ public class Problem6 {
                 break;
             case "*":
                 while (count > 0) {
-                    num1 = randomNumbers.nextInt(100);
-                    num2 = randomNumbers.nextInt(100);
+                    num1 = randomNumbers.nextInt(10);
+                    num2 = randomNumbers.nextInt(10);
                     result = num1 * num2;
                     System.out.print(num1 + " * " + num2 + " = ");
-                    int userResult = input.nextInt();
+                    userResult = input.nextInt();
                     if (userResult == result) {
                         count--;
                         System.out.println("Correct. " + count + " tries left.");
@@ -79,8 +75,48 @@ public class Problem6 {
                     }
                 }
                 break;
+            case "/":
+                while (count > 0) {
+                    num1 = randomNumbers.nextInt(10);
+                    num2 = randomNumbers.nextInt(10);
+
+                    if (num2 == 0) {
+                        num2 = randomNumbers.nextInt(10);
+                    }
+
+                    while (num1 < num2) {
+                        num1 = randomNumbers.nextInt(10);
+                        if (num1 >= num2) {
+                            break;
+                        }
+                    }
+
+                    result = num1 / num2;
+                    System.out.print(num1 + " / " + num2 + " = ");
+                    
+                    // Get the user's answer
+                    // Check if the division has a remainder
+                    userResult = input.nextInt();
+                    divisionRemainder = num1 % num2;
+
+                    if (userResult == result) {
+                        if (divisionRemainder != 0) {
+                            System.out.print("Please enter the remainder: ");
+                            userRemainder = input.nextInt();
+                            if (userRemainder == divisionRemainder) {
+                                count--;
+                                System.out.println("Correct. " + count + " tries left.");
+                            }
+                        } else {
+                            count--;
+                            System.out.println("Correct. " + count + " tries left.");
+                        }                    
+                    } else {
+                        System.out.println("Incorrect." + count + " tries left.");
+                    }
+                }
+                break;
             default:
-                System.out.println("Please input the right symbols (+,-,*,/)");
                 break;
         }
 
